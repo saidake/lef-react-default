e10b08bc23c5a22d06fd1225227a837ae32144f3
zouhao <saidake@qq.com>
2021-06-23 14:20:53 +0800

    first

---
+++ template/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.scss';
+
+const template = React.createElement('div',null,'This is a lef project');
+
+ReactDOM.render(template, document.getElementById('root'));
